9c39aedf5d982cfc2350d5ae9d7856a69478af0c
Sharmaharshit0 <harshit.sharma_cs.aiml24@gla.ac.in>
2026-02-27 23:42:15 +0530

    add ultrasonic distance measurement functionality

--- src/main.cpp
+++ src/main.cpp
@@ -47,6 +47,16 @@ void setup() {
 
 void loop() {
 
+
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration * 0.034 / 2;
     // TODO 9:
     // Set TRIG LOW for 2 microseconds
 
